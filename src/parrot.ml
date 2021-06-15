7d73f24e74aae6fc0ac34c127115bb21acea9454
Blake Loring <blake@parsed.uk>
2021-06-15 13:13:00 +0800

    Index generation

--- src/parrot.ml
+++ src/parrot.ml
@@ -6,15 +6,34 @@ exception InvalidArticleName of string
 
 let out_article_name = function
   | "" -> raise (InvalidArticleName "all whitespace")
-  | name -> name |> String.substr_replace_all ~pattern:" " ~with_:"_"  |> String.substr_replace_all ~pattern:"-" ~with_:"slash" |> String.substr_replace_all ~pattern:"," ~with_:"comma" |> String.substr_replace_all ~pattern:"'" ~with_:"single_quote" |> String.substr_replace_all ~pattern:"\"" ~with_:"quote"
+  | name ->
+    name
+    |> String.substr_replace_all ~pattern:" " ~with_:"_"
+    |> String.substr_replace_all ~pattern:"-" ~with_:"_"
+    |> String.substr_replace_all ~pattern:"," ~with_:"_"
+    |> String.substr_replace_all ~pattern:"'" ~with_:""
+    |> String.substr_replace_all ~pattern:"\"" ~with_:""
+    |> String.substr_replace_all ~pattern:":" ~with_:""
+    |> String.substr_replace_all ~pattern:"." ~with_:"_"
+    |> String.substr_replace_all ~pattern:"&" ~with_:"and"
 ;;
+
+let out_index_path = sprintf "%s/index.html"
 
 let out_article_path article output_base =
   sprintf "%s/%s.html" output_base (out_article_name article.name)
 ;;
 
 let emit_article article article_template output_folder =
-  Out_channel.write_all (out_article_path article output_folder) ~data:(render_article_template article article_template)
+  Out_channel.write_all
+    (out_article_path article output_folder)
+    ~data:(render_article article article_template)
+;;
+
+let emit_index base output_folder =
+  Out_channel.write_all
+    (out_index_path output_folder)
+    ~data:(render_index (load_index_template base))
 ;;
 
 let%test "directory_scan" =
@@ -28,6 +47,7 @@ let%test "directory_scan" =
       emit_article article article_template output_base;
       print_list xs
   in
+  emit_index base output_base;
   print_list (Articles.all_articles base);
   true
 ;;
--- src/template.ml
+++ src/template.ml
@@ -8,19 +8,32 @@ let article_template base = sprintf "%s/article.html" (templates_subdir base)
 (* The nav bar at the top is static but appears in multiple different templates. Because of this we pull it out into it's own template file
   * TODO: It would be more powerful to use a template-schema like ${{{file:/templates/nav.html}}} across all templates in the future. *)
 let nav_template base = sprintf "%s/nav.html" (templates_subdir base)
+let index_template base = sprintf "%s/index.html" (templates_subdir base)
 
 (* Replace a template identifier like ${{{HELLO}}} by a substituted value *)
 let template_replace template_id fragment =
   String.substr_replace_first ~pattern:("${{{" ^ template_id ^ "}}}") ~with_:fragment
 ;;
 
+let load_index_template base =
+  let nav_template = In_channel.read_all (nav_template base) in
+  let index_template = In_channel.read_all (index_template base) in
+  template_replace "NAV_BAR_CONTENT" nav_template index_template
+;;
+
 let load_article_template base =
   let nav_template = In_channel.read_all (nav_template base) in
-  template_replace "NAV_BAR_CONTENT" nav_template (In_channel.read_all (article_template base))
+  template_replace
+    "NAV_BAR_CONTENT"
+    nav_template
+    (In_channel.read_all (article_template base))
+;;
 
-let render_article_template article template =
+let render_article article template =
   template
   |> template_replace "ARTICLE_CONTENT" (Article.to_html article)
   |> template_replace "ARTICLE_TITLE" (Article.title_to_html article)
   |> template_replace "ARTICLE_TIME" (Article.created_time_to_html article)
 ;;
+
+let render_index template = template
